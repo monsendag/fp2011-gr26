566fa6b27bb0c9c47347141bf411f15a5be21a47
Dag <me@dag.im>
2011-04-04 16:23:01 +0000

    changes to the calendar panel stuff

--- src/fp/client/gui/calendar/CalendarModel.java
+++ src/fp/client/gui/calendar/CalendarModel.java
@@ -1,23 +1,51 @@
 package fp.client.gui.calendar;
 
+import java.util.List;
+
+import no.ntnu.fp.model.Activity;
+
 import org.joda.time.DateTime;
 
 public class CalendarModel {
 	
-	private int week;
+	private DateTime monday;
+	private List<Activity> activities;
+	
+	
+	public CalendarModel() {
+		setCurrentWeek();
+	}
+	
 
 	public int getWeekNumber() {
-		return week;
+		return monday.getWeekOfWeekyear();
 	}
 
-	public void setWeekNumber(int weekNumber) {
-		this.week = weekNumber;
-	}
-	
 	public void setCurrentWeek() {
-		week = new DateTime().getWeekOfWeekyear();
+		DateTime now = new DateTime();
+		monday = now.minusDays((now.getDayOfWeek()-1));  // monday == 1
 	}
 	
+	public DateTime getMonday() {
+		return monday;
+	}
+
+
+	public void setMonday(DateTime monday) {
+		this.monday = monday;
+	}
+
+
+	public List<Activity> getActivities() {
+		return activities;
+	}
+
+
+	public void setActivities(List<Activity> activities) {
+		this.activities = activities;
+	}
+
+
 	public void setNextWeek() {
 		
 	}
@@ -25,5 +53,8 @@ public class CalendarModel {
 	public void setPreviousWeek() {
 		
 	}
-
+	
+	public boolean inWeek(DateTime time) {
+		return time.getYear() == getMonday().getYear() && time.getWeekOfWeekyear() == getMonday().getWeekOfWeekyear();
+	}
 }

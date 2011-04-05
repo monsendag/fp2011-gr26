625e117dae33d605c79c2cc62019fbb4e0c28b4c
monsen.dag@gmail.com <monsen.dag@gmail.com@bd16c86f-1db5-4fa5-21a3-b60b162687d9>
2011-04-05 13:42:09 +0000

    added client storage activityStorage

---
+++ trunk/src/fp/client/storage/ActivityStorage.java
@@ -0,0 +1,14 @@
+package fp.client.storage;
+
+import no.ntnu.fp.model.Activity;
+
+public class ActivityStorage {
+	
+	
+	
+	
+	public static void createActivity(Activity a) {
+		// store and send over network
+	}
+	
+}

d19ba61ce39e7bc83b0dddc657d32c0b7f1119cb
Dag <me@dag.im>
2011-04-05 12:42:56 +0000

    changes to server side storage code

--- src/no/ntnu/fp/storage/Storage.java
+++ src/no/ntnu/fp/storage/Storage.java
@@ -1,27 +1,28 @@
-/*
- * Created on Sep 29, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package no.ntnu.fp.storage;
 
-import java.io.IOException;
-import java.text.ParseException;
-import java.net.URL;
+import java.util.HashMap;
 
-import no.ntnu.fp.model.Project;
+import no.ntnu.fp.model.Activity;
+import no.ntnu.fp.model.Employee;
+import no.ntnu.fp.model.Meeting;
+import no.ntnu.fp.model.Room;
 
-/**
- * @author tho
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-public interface Storage {
-
-	Project load(URL url) throws IOException, ParseException;
+public class Storage {
+	private static Storage instance;
 	
-	void save(URL url, Project project) throws IOException, ParseException;
-
+	public static volatile HashMap<String,Employee> empCache;
+	public static volatile HashMap<Integer,Room> roomCache;
+	public static volatile HashMap<Integer,Activity> actCache;
+	public static volatile HashMap<Integer,Meeting> mtngCache;
+	
+	private DBConnection connection;
+	
+	private Storage() {
+		connection = new DBConnection();
+	}
+	
+	public static Storage getInstance() {
+		if(instance == null) instance = new Storage();
+		return instance;
+	}
 }

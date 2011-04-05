a60cba1cb87cfb986e5fcd94c5322e1f88b755e6
monsen.dag@gmail.com <monsen.dag@gmail.com@bd16c86f-1db5-4fa5-21a3-b60b162687d9>
2011-04-05 18:36:01 +0000

    cleaned up network code

--- trunk/src/fp/common/network/ClientConnection.java
+++ trunk/src/fp/common/network/ClientConnection.java
@@ -1,39 +1,19 @@
 package fp.common.network;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 import fp.common.models.Employee;
 import fp.common.models.XmlSerializer;
 
 
 public class ClientConnection extends Connection {
-	
-	protected InetAddress hostname;
-	
 	public ClientConnection() throws IOException {
-		hostname = InetAddress.getLocalHost();
-		socket = new Socket(hostname, serverPort);
-		// get the IO streams
-		InputStream is = socket.getInputStream();
-		OutputStream os = socket.getOutputStream();
-
-		// Set up input stream readers
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader in = new BufferedReader(isr);
-		OutputStreamWriter out = new OutputStreamWriter(os);
-
+		super(InetAddress.getLocalHost());
 		
 		Employee arne = new Employee("Arne bjarne", "arne@bjarne.no", "shubidubidu");
 		String xml = XmlSerializer.getInstance().serialize(arne);
+		
 		out.write(xml+EOL);
 		out.flush();
 		

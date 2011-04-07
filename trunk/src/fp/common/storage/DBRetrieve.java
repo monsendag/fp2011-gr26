c05306bd701842669358e53477d4fa188eaba954
tobias.str@gmail.com <tobias.str@gmail.com@bd16c86f-1db5-4fa5-21a3-b60b162687d9>
2011-04-07 11:14:26 +0000

    oh teh errrrrrrorz

--- trunk/src/fp/common/storage/DBRetrieve.java
+++ trunk/src/fp/common/storage/DBRetrieve.java
@@ -181,7 +181,7 @@ public class DBRetrieve {
 			Message m;
 			while(rs.next()) {
 				m = new Message();
-				m.setMessageID(rs.getInt("meetingID"));
+				m.setMessageID(rs.getInt("messageID"));
 				m.isInvitation(rs.getBoolean("isinvite"));
 				m.isRead(rs.getBoolean("isread"));
 				m.setCreatedOn(new DateTime(rs.getTimestamp("time").getTime()));
@@ -679,7 +679,7 @@ public class DBRetrieve {
 				m.setDescription(rs.getString("message"));
 				m.setEmployee(getEmployee(username));
 				m.setMeeting(getMeeting(rs.getInt("activityID")));
-				m.setMessageID(rs.getInt("meetingID"));
+				m.setMessageID(rs.getInt("messageID"));
 				m.isInvitation(rs.getBoolean("isinvite"));
 				m.isRead(rs.getBoolean("isread"));
 				messages.add(m);
@@ -727,7 +727,7 @@ public class DBRetrieve {
 				m.setDescription(rs.getString("message"));
 				m.setEmployee(getEmployee(username));
 				m.setMeeting(getMeeting(rs.getInt("activityID")));
-				m.setMessageID(rs.getInt("meetingID"));
+				m.setMessageID(rs.getInt("messageID"));
 				m.isInvitation(rs.getBoolean("isinvite"));
 				m.isRead(rs.getBoolean("isread"));
 				messages.add(m);
